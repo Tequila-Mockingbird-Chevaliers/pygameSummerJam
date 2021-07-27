ee58d04fc20375e7d184af6e349a24ef7b4db06d
Grimmys <funkyzermmy@gmail.com>
2021-07-27 15:38:51 +0200

    Introduced the notion of block size
    Added new constants for block size, and brick size that is for now equal to block size.
    Switch brick width from 50 to 30.
    Added the possibility to rescale an image.

--- src/entities/brick.py
+++ src/entities/brick.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import random
 
+from src.constants import BRICK_WIDTH, BRICK_HEIGHT
 from src.entities.game_object import GameObject
 from src.state.game_state import GameState
 
@@ -16,7 +17,7 @@ class Brick(GameObject):
         Initialize Brick class
         """
         super().__init__(game_state, random.choice(game_state.assets.bricks))
-        self.rect.topleft = (grid[0] * 50, 300 + grid[1] * 20)
+        self.rect.topleft = (grid[0] * BRICK_WIDTH, 300 + grid[1] * BRICK_HEIGHT)
 
     def remove(self):
         """
